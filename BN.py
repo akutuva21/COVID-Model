79ae5214413514b9248d9da10d87055ea99a4703
Achyudhan <akutuva22@gmail.com>
2022-08-25 18:13:48 -0400

    Small edits to IL6/STAT3 pathways

--- BN.py
+++ BN.py
@@ -36,8 +36,6 @@ def BN(components, n_iter):
             if (i == "ADAM_17"):
                 # A2_t1r activates ADAM 17, which promotes ACE2 shedding (https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8185693/")
                 x[i] = x["ANG_2_T1R"]
-            if (i == "SIL6R"):
-                x[i] = x["ADAM_17"]
                 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7224649/
             if (i == "TLR4"):
                 x[i] = x["Virus"]
@@ -47,81 +45,75 @@ def BN(components, n_iter):
                 x[i] = x["Viral_Repl"]
                 # antiviral activity of RIG-1 may comprise inhibition of viral entry into the host cell by preventing the expression of its receptor, ACE2
                 # https://www.news-medical.net/news/20210215/RIG-1-like-receptors-may-play-dominant-role-in-suppressing-SARS-CoV-2-infection.aspx
-            if (i == "NFKβ"):
-                x[i] = not x["IKKB α/β"] # x["ANG_2_T1R"] or x["PKC"] or x["RIG1"] or
-                # should be good, may need to find what exactly inhibits NFKβ
-                # common drug therapy is inhibiting NFKβ (https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7648206/")
-
-            ##################### NEW #####################
-            if (i == "IKKB α/β"):
-                x[i] = not (x["TLR4"] or x["TNFR"] or x["IL1R"] or x["RIG1"])
-            ##################### NEW #####################
-
-            if (i == "TNF"):
-                x[i] = x["NFKβ"]
-                # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7114322/
+            if (i == "NFKB"):
+                x[i] = not x["IKKB a/b"] # x["ANG_2_T1R"] or x["PKC"] or x["RIG1"] or
+                # should be good, may need to find what exactly inhibits NFKB
+                # common drug therapy is inhibiting NFKB (https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7648206/")
             if (i == "IRF3"):
                 x[i] = x["RIG1"]# and not x["Viral_Repl"]
                 # https://journals.asm.org/doi/10.1128/CMR.00299-20
                 # SARS-CoV-2 membrane protein binds to importin karyopherin subunit alpha-6 (KPNA6") to inhibit interferon regulatory factor 3(IRF3") nuclear translocation
                 # https://www.frontiersin.org/articles/10.3389/fcimb.2021.766922/full
             if (i == "STAT1"):
-                x[i] = x["IFNR"]# and not x["Viral_Repl"]
+                x[i] = x["IFNR"] # and not x["Viral_Repl"]
                 # After the infection, STAT1 activity is inhibited by the SARS-CoV-2 proteins, NSP1, and ORF6
                 # https://www.nature.com/articles/s41418-020-00633-7
-
-            ########################## NEW ###############################
+            
+            ##### NEW #####
             if (i == "STAT3"):
-                #x[i] = x["IL6"] # IL6 is the main contributor to STAT3 - add il6 and il6r to the list of components
-                x[i] = x[i]
+                x[i] = x["IL6R"] #or (x["SIL6R"] and x["IL6"]) # IL6 is the main contributor to STAT3 - add il6 and il6r to the list of components
                 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7937040/
-            ########################## NEW ###############################
+            if (i == "IL6"):
+                x[i] = x["NFKB"]
+            if (i == "IL6R"):
+                x[i] = x["IL6"]
+                # https://www.kegg.jp/pathway/map05171
+            if (i == "SIL6R"):
+                x[i] = x["ADAM_17"] #or x["IL6"]
+            if (i == "IKKB a/b"):
+                x[i] = not (x["TLR4"] or x["IL1R"] or x["RIG1"] or x["TNFR"]) # all phosphorylate the complex to release ikkb
+            if (i == "TNF"):
+                x[i] = x["ADAM_17"] or x["NFKB"]
+                # https://www.kegg.jp/pathway/map05171
+            ##### NEW #####
 
             if (i == "ISG"):
                 x[i] = x["STAT1"]
                 # https://www.nature.com/articles/s41586-021-03234-7
             if (i == "C_FLIP"):
-                x[i] = x["NFKβ"] and not x["FOXO3A"]
+                x[i] = x["NFKB"] and not x["FOXO3A"]
                 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4219646/
-            if (i == "INF α/β"):
+            if (i == "INF a/b"):
                 x[i] = x["IRF3"]# and not x["Viral_Repl"]
                 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7995242/
             if (i == "NRLP3"):
-                x[i] = x["NFKβ"]
+                x[i] = x["NFKB"]
                 # https://www.nature.com/articles/ni.3772
             if (i == "CASP1"):
                 x[i] = x["NRLP3"]
                 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6651423/
-
-            ########################## CHANGED ###############################
             if (i == "FOXO3A"):
                 #x[i] = x["Virus"]
-                x[i] = (x["ROS"] or x["STAT3"]) and not x["IKKB α/β"]
+                x[i] = (x["ROS"] or x["STAT3"]) and not x["IKKB a/b"]
                 # no direct relation (drug targetting in place - https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8187014/")
-            ########################## CHANGED ###############################
-
             if (i == "IFNR"):
-                x[i] = x["INF α/β"]
+                x[i] = x["INF a/b"]
                 # need to confirm more
                 # https://www.frontiersin.org/articles/10.3389/fimmu.2020.606456/full
             if (i == "BCL_2"):
-                x[i] = x["NFKβ"]
+                x[i] = x["NFKB"]
                 # https://www.nature.com/articles/1204926
             if (i == "TBid"):
                 x[i] = x["CASP8"]
                 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4882451/
             if (i == "Bax_Bak"):
-                x[i] = x["BCL_2"] or x["TBid"]
+                x[i] = not x["BCL_2"] and x["TBid"]
                 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4219646/
             if (i == "CASP9"):
                 x[i] = x["Bax_Bak"]
                 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4219646/
-
-            ########################## CHANGED ###############################
             if (i == "ROS"):
                 x[i] = x["ROS"] #not x["FOXO3A"]
-            ########################## CHANGED ###############################
-
             if (i == "TNFR"):
                 x[i] = x["TNF"]
                 # Do more research later
@@ -134,19 +126,19 @@ def BN(components, n_iter):
                 x[i] = x["CASP1"]
                 # https://www.nature.com/articles/s41467-019-09753-2
             if (i == "IL1"):
-                x[i] = x["MLKL"] or x["NFKβ"]
+                x[i] = x["MLKL"] or x["NFKB"]
                 # Look into this more later
                 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5321867/
             if (i == "IL1R"):
                 x[i] = x["IL1"]
             if (i == "MLKL"):
-                x[i] = x["RIPK1_3"] #and not (x["NRLP3"]")# or x["CASP1"]")
+                x[i] = x["RIPK1&3"] #and not (x["NRLP3"]")# or x["CASP1"]")
                 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5321867/
                 # read more of this later
             if (i == "Necroptosis"):
                 x[i] = x["MLKL"]
                 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5321867/
-            if (i == "RIPK1_3"):
+            if (i == "RIPK1&3"):
                 x[i] = (x["RIG1"] or x["TLR4"] or x["STAT1"]) and not x["CASP8"]
                 # read more later
                 # https://www.sciencedirect.com/science/article/pii/S1097276514008661
@@ -162,13 +154,13 @@ def BN(components, n_iter):
 
 def main():
     random.seed(0)
-    components = ["Virus","Viral_Repl","ACE2","PKC","ANG_2_T1R","ANG_2","ADAM_17","SIL6R","TLR4","RIG1","NFKβ","IKKB α/β", "TNF","IRF3","STAT1","STAT3", "ISG","C_FLIP","INF α/β","NRLP3","CASP1","FOXO3A","IFNR","BCL_2","TBid","Bax_Bak","CASP9","ROS","TNFR","FADD","Pyroptosis","IL1","IL1R","MLKL","Necroptosis","RIPK1_3","CASP8","Apoptosis"]
+    components = ["Virus","Viral_Repl","ACE2","PKC","ANG_2_T1R","ANG_2","ADAM_17","SIL6R","TLR4","RIG1","NFKB","IKKB a/b", "TNF","IRF3","STAT1","STAT3", "IL6", "IL6R","ISG","C_FLIP","INF a/b","NRLP3","CASP1","FOXO3A","IFNR","BCL_2","TBid","Bax_Bak","CASP9","ROS","TNFR","FADD","Pyroptosis","IL1","IL1R","MLKL","Necroptosis","RIPK1&3","CASP8","Apoptosis"]
     #components.sort()
 
     # find experiments that do this to confirm (grid search of genes array)
 
     n_iter = 25
-    orders = 200
+    orders = 100
     mat = np.zeros((n_iter + 1, len(components)))
     # consider simplification in the future, add negative feedback loops
 
@@ -191,7 +183,8 @@ def main():
                                                             cmap(np.linspace(minval, maxval, n)))
         return new_cmap
 
-    ax = sns.heatmap(mat, cmap = truncate_colormap(cmap,0.25, 0.75,n=200), linewidths=.05, xticklabels=components,
+    edited_comp = [x.replace("_", " ").replace("a/b", "α/β") for x in components]
+    ax = sns.heatmap(mat, cmap = truncate_colormap(cmap,0.25, 0.75,n=200), linewidths=.05, xticklabels=edited_comp,
                      yticklabels=yticklabels, vmin=0, vmax=1, alpha = 0.90)
     ax.tick_params(axis='y', which='major', labelsize= 10)
 
